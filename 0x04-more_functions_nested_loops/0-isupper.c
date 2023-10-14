326bd1573d7bb3a8fb7c9e25f5e44f5b1766cdc2
chaimaa <chaimaamiftah13@gmail.com>
2023-10-13 18:11:55 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -prog cheks for uppercase character
+ * @c : Variable text
+ * Return : Always 0.
+ */
+int _issuper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+} 
