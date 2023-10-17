dde03b688a597dea2d7696915b3557d4b514f938
chaimaa <chaimaamiftah13@gmail.com>
2023-10-17 15:23:31 -0700

    1234

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int_putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a ,int *b);
+int_strlen(char *s);
+int_puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *s);
+void puts_half(char *s);
+void print_array(int *a, int *b);
+char *_strcpy(char *dest, char *src);
+
+#endif
